120f287a05c1e5486d3d1e43768594b115fbd30d
Sahil <sahilch2839@gmail.com>
2025-10-03 18:48:52 +0530

    add cron job for bills

--- accounts/management/commands/generate_bills.py
+++ accounts/management/commands/generate_bills.py
@@ -1,10 +1,25 @@
-# accounts/management/commands/generate_bills.py
 from django.core.management.base import BaseCommand
-from accounts.scheduler import generate_bills  # import your function
+from accounts.scheduler import generate_bills
+import time
 
 class Command(BaseCommand):
     help = "Generate monthly/daily bills for tenants"
 
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--test', action='store_true', help='Run in test mode (every 10s)'
+        )
+
     def handle(self, *args, **options):
-        generate_bills()
-        self.stdout.write(self.style.SUCCESS("Bills generated successfully"))
+        test_mode = options.get('test', False)
+
+        if test_mode:
+            # Loop 6 times every 10 seconds (for testing)
+            for _ in range(6):
+                generate_bills()
+                time.sleep(10)
+            self.stdout.write(self.style.SUCCESS("Bills generated in test mode."))
+        else:
+            # Normal execution for production
+            generate_bills()
+            self.stdout.write(self.style.SUCCESS("Bills generated successfully."))
